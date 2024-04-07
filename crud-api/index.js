a9089af3b17be0e96decd5d583aa0d5156a1a2ff
dogukancaner <dogukanncanerr@gmail.com>
2024-04-07 17:35:29 +0300

    required packages installed

---
+++ crud-api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node API");
+});
+
+mongoose
+    .connect(
+        "mongodb+srv://dogukanncanerr:Z8WxtbaHWKkRYrJj@backenddb.vfgbh2b.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+    )
+    .then(() => {
+        console.log("Connected to database!");
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    });
+
