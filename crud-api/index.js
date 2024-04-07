ce7c80f805b9ce9e6efe6b61190deef2d1a01b2b
dogukancaner <dogukanncanerr@gmail.com>
2024-04-07 18:08:48 +0300

    create api

--- crud-api/index.js
+++ crud-api/index.js
@@ -9,7 +9,24 @@ app.get("/", (req, res) => {
     res.send("Hello from Node API");
 });
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products);
 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.post("/api/products", async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 mongoose
     .connect(
         "mongodb+srv://dogukanncanerr:Z8WxtbaHWKkRYrJj@backenddb.vfgbh2b.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
