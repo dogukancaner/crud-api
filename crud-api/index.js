aee5dd86e51b658284cfc120e627f25d6ef99c47
dogukancaner <dogukanncanerr@gmail.com>
2024-04-07 18:24:45 +0300

    create update api

--- crud-api/index.js
+++ crud-api/index.js
@@ -41,7 +41,20 @@ app.get("/api/product/:id", async (req, res) => {
     }
 })
 
+app.put("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 
 app.post("/api/products", async (req, res) => {
     try {
